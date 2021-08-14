595cc3eee568fe1de4849e75b198b115d824da39
autodotua <autodotua@outlook.com>
2021-08-14 11:00:52 +0800

    后端新增FTP支持

---
+++ SimpleFFmpegGUI.WebAPI/Controllers/FtpController.cs
@@ -0,0 +1,71 @@
+﻿using Furion.FriendlyException;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SimpleFFmpegGUI.Dto;
+using SimpleFFmpegGUI.Model;
+using SimpleFFmpegGUI.WebAPI.Dto;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleFFmpegGUI.WebAPI.Controllers
+{
+    public class FtpController : FFmpegControllerBase
+    {
+        private FtpManager inputFtp;
+        private FtpManager outputFtp;
+
+        public FtpController(ILogger<MediaInfoController> logger,
+            IConfiguration config) : base(logger, config) { }
+
+        [HttpPost]
+        [Route("Input/On")]
+        public async Task OpenInput(int id)
+        {
+            if (inputFtp != null)
+            {
+                return;
+            }
+            inputFtp = new FtpManager(GetInputDir(), config.GetValue<int>("FtpPort", FtpManager.FreeTcpPort()));
+            await inputFtp.StartAsync();
+        }
+
+        [HttpPost]
+        [Route("Input/Off")]
+        public async Task CloseInput(int id)
+        {
+            if (inputFtp == null)
+            {
+                return;
+            }
+            await inputFtp.StopAsync();
+            inputFtp = null;
+        }
+
+        [HttpPost]
+        [Route("Output/On")]
+        public async Task OpenOutput(int id)
+        {
+            if (outputFtp != null)
+            {
+                return;
+            }
+            outputFtp = new FtpManager(GetOutputDir(), config.GetValue<int>("FtpPort", FtpManager.FreeTcpPort()));
+            await outputFtp.StartAsync();
+        }
+
+        [HttpPost]
+        [Route("Output/Off")]
+        public async Task CloseOutput(int id)
+        {
+            if (outputFtp == null)
+            {
+                return;
+            }
+            await outputFtp.StopAsync();
+            outputFtp = null;
+        }
+    }
+}
---
+++ SimpleFFmpegGUI.WebAPI/FtpManager.cs
@@ -0,0 +1,86 @@
+﻿using FubarDev.FtpServer;
+using FubarDev.FtpServer.FileSystem.DotNet;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleFFmpegGUI.WebAPI
+{
+    public class FtpManager : IDisposable
+    {
+        public FtpManager(string path, int port)
+        {
+            var services = new ServiceCollection();
+
+            // use %TEMP%/TestFtpServer as root folder
+            services.Configure<DotNetFileSystemOptions>(opt => opt
+                .RootPath = path);
+
+            // Add FTP server services
+            // DotNetFileSystemProvider = Use the .NET file system functionality
+            // AnonymousMembershipProvider = allow only anonymous logins
+            services.AddFtpServer(builder => builder
+                .UseDotNetFileSystem() // Use the .NET file system functionality
+                .EnableAnonymousAuthentication()); // allow anonymous logins
+
+            // Configure the FTP server
+            services.Configure<FtpServerOptions>(opt => opt.Port = port);
+
+            // Build the service provider
+            serviceProvider = services.BuildServiceProvider();
+            // Initialize the FTP server
+            ftpServerHost = serviceProvider.GetRequiredService<IFtpServerHost>();
+            Path = path;
+            Port = port;
+        }
+
+        private readonly IFtpServerHost ftpServerHost;
+        private readonly ServiceProvider serviceProvider;
+
+        public string Path { get; }
+        public int Port { get; }
+
+        public Task StartAsync()
+        {
+            if (ftpServerHost == null)
+            {
+                throw new NullReferenceException("请先初始化");
+            }
+            return ftpServerHost.StartAsync(CancellationToken.None);
+        }
+
+        public Task StopAsync()
+        {
+            if (ftpServerHost == null)
+            {
+                throw new NullReferenceException("请先初始化");
+            }
+            return ftpServerHost.StopAsync(CancellationToken.None);
+        }
+
+        public static int FreeTcpPort()
+        {
+            TcpListener l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            int port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
+            return port;
+        }
+
+        public void Dispose()
+        {
+            serviceProvider?.Dispose();
+        }
+
+        ~FtpManager()
+        {
+            Dispose();
+        }
+    }
+}
