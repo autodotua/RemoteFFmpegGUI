2e2ee48a82e5c18daf7b682da00bc1c247d2a39d
autodotua <autodotua@outlook.com>
2021-08-21 09:16:52 +0800

    新增支持Host设置开机自启，支持设置工作目录为当前目录

--- SimpleFFmpegGUI.Host/Program.cs
+++ SimpleFFmpegGUI.Host/Program.cs
@@ -8,16 +8,27 @@ using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Logging.Console;
 using SimpleFFmpegGUI.Model;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace SimpleFFmpegGUI
 {
     internal class Options
     {
-        [Option('p', "pipename", Required = false, HelpText = "命名管道名称")]
+        [Option('p', Required = false, HelpText = "命名管道名称")]
         public string PipeName { get; set; }
+
+        [Option('s', Default = false, Required = false, HelpText = "注册开机启动")]
+        public bool RegisterStartup { get; set; }
+
+        [Option('u', Default = false, Required = false, HelpText = "取消开机启动")]
+        public bool UnregistereStartup { get; set; }
+
+        [Option('d', Default = false, Required = false, HelpText = "设置工作目录为程序所在目录")]
+        public bool WorkingDirectoryHere { get; set; }
     }
 
     public class Program
@@ -28,7 +39,7 @@ namespace SimpleFFmpegGUI
         {
             string pipeName = DefaultPipeName;
             Parser.Default.ParseArguments<Options>(args)
-                     .WithParsed<Options>(o =>
+                     .WithParsed(o =>
                      {
                          if (o.PipeName != null)
                          {
@@ -37,7 +48,60 @@ namespace SimpleFFmpegGUI
                          }
                          else
                          {
-                             Console.WriteLine($"管道名未设置，默认为为： {DefaultPipeName}");
+                             Console.WriteLine($"管道名未设置，默认为： {DefaultPipeName}");
+                         }
+                         if (o.RegisterStartup)
+                         {
+                             if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                             {
+                                 throw new PlatformNotSupportedException("开机自启仅支持Windows");
+                             }
+                             if (FzLib.Program.Startup.IsRegistryKeyExist() != FzLib.IO.ShortcutStatus.Exist)
+                             {
+                                 List<string> args = new List<string>();
+                                 if (o.WorkingDirectoryHere)
+                                 {
+                                     args.Add("-d");
+                                 }
+                                 if (o.PipeName != null)
+                                 {
+                                     args.Add("-p");
+                                     args.Add(o.PipeName);
+                                 }
+                                 FzLib.Program.Startup.CreateRegistryKey(string.Join(' ', args));
+                                 Console.WriteLine("已注册开机自启，参数为" + string.Join(' ', args));
+                             }
+                             else
+                             {
+                                 Console.WriteLine("已经是开机自启，无需注册");
+                             }
+                             Console.WriteLine("按任意键退出");
+                             Console.ReadKey();
+                             Environment.Exit(0);
+                         }
+                         if (o.UnregistereStartup)
+                         {
+                             if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                             {
+                                 throw new PlatformNotSupportedException("开机自启仅支持Windows");
+                             }
+                             if (FzLib.Program.Startup.IsRegistryKeyExist() == FzLib.IO.ShortcutStatus.Exist)
+                             {
+                                 FzLib.Program.Startup.DeleteRegistryKey();
+                                 Console.WriteLine("已取消开机自启");
+                             }
+                             else
+                             {
+                                 Console.WriteLine("未注册开机自启，无需取消");
+                             }
+                             Console.WriteLine("按任意键退出");
+                             Console.ReadKey();
+                             Environment.Exit(0);
+                         }
+                         if (o.WorkingDirectoryHere)
+                         {
+                             FzLib.Program.App.SetWorkingDirectoryToAppPath();
+                             Console.WriteLine("工作目录设置为程序目录：" + FzLib.Program.App.ProgramDirectoryPath);
                          }
                      });
             ConsoleLogger.StartListen();
